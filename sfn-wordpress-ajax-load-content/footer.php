43885e86a05f11c66a00a24119f28e983f415c50
Curtis McHale <curtis@curtismchale.ca>
2010-10-08 21:41:16 -0700

    started to flesh out the header and footer

--- sfn-wordpress-ajax-load-content/footer.php
+++ sfn-wordpress-ajax-load-content/footer.php
@@ -1,11 +1,11 @@
-        </div><!-- /#main-wrapper -->
 
         <footer>
-
+            <p class="site-credit">This tutorial theme built by <a href="http://curtismchale" title="SFNdesign">SFNdesign</a> and proudly powered by <a href="http://wordpress.org" title="WordPress">WordPress</a>.</p>
         </footer>
 
-        <?php wp_footer(); ?>
+    </div><!-- /#main-wrapper -->
 
-    </body>
-    
+    <?php wp_footer(); ?>
+
+</body>
 </html>
--- sfn-wordpress-ajax-load-content/header.php
+++ sfn-wordpress-ajax-load-content/header.php
@@ -1,24 +1,24 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
     
-    <head>
-        
-        <title><?php the_title(); ?></title>
-        
-        <?php wp_head(); ?>
-        
-    </head>
+<head>
     
-    <body <?php body_class(); ?>>
-        
+    <title><?php the_title(); ?></title>
+    
+    <?php wp_head(); ?>
+    
+</head>
+    
+<body <?php body_class(); ?>>
+    
+    <div id="main-wrapper">
+    
         <header>
-            
-        </header>
-        
-        <div id="main-wrapper">
             
             <nav>
                 <!--
                     TODO add wp 3.0 navigation
                 -->
             </nav>
+            
+        </header>
